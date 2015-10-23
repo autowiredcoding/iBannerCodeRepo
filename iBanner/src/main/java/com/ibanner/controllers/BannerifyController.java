6b4ab52c85a080dea08e04dfd11d0a43a57de379
Shyam Ramath <shyamu.in@gmail.com>
2015-10-23 16:49:26 -0500

    Initail Commit

---
+++ iBanner/src/main/java/com/ibanner/controllers/BannerifyController.java
@@ -0,0 +1,31 @@
+package com.ibanner.controllers;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.ibannerify.external.IBannerify;
+
+
+
+public class BannerifyController {
+	
+	@Autowired
+	IBannerify iBannerify;
+	
+	@RequestMapping(value = "/banner", method = RequestMethod.GET)
+	@ResponseBody
+	void getBanner(HttpServletRequest request, HttpServletResponse httpServletResponse) throws IOException{
+		httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
+		httpServletResponse.setHeader("Access-Control-Allow-Methods", "*");
+	    httpServletResponse.setContentType("image/jpeg");
+	    httpServletResponse.getOutputStream().write(IOUtils.toByteArray(iBannerify.getBannerImage()));
+	}
+}
---
+++ iBanner/src/main/java/com/ibannerify/external/IBannerify.java
@@ -0,0 +1,9 @@
+package com.ibannerify.external;
+
+import java.io.InputStream;
+
+public interface IBannerify {
+	
+	InputStream getBannerImage();
+
+}
